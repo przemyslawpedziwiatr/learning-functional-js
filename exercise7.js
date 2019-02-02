31a1b27a431dae822ef631b14d272cf23f8f21cb
pp <przemyslaw.pedziwiatr@gmail.com>
2019-02-02 11:21:31 +0100

    More elegant solution to reduce implementation

--- exercise7.js
+++ exercise7.js
@@ -1,15 +1,22 @@
 function reduce(arr, fn, initial) {
-    // fn = prev, curr, index, arr
-    console.log({arr, initial});
-    if(!arr.length) return initial;
-    const length = arr.length;
-    const toOperate = arr[0];
-    const nextElement = arr.slice(1);
-    let index = 0;
-    const result = fn(
-        initial, toOperate, ++index, arr
-    );
-    return reduce(nextElement, fn, initial);
+    // if(!arr.length) return initial;
+    // const length = arr.length;
+    // const toOperate = arr[0];
+    // const nextElement = arr.slice(1);
+    // let index = 0;
+    // const result = fn(
+    //     initial, toOperate, ++index, arr
+    // );
+    // return reduce(nextElement, fn, initial);
+
+    return (
+        function reduceOne(index, value) {
+            if(index > arr.length - 1) return value;
+            return reduceOne(index + 1, fn(
+                value, arr[index], index, arr
+            ));
+        }
+    )(0, initial);
 }
 
 module.exports = reduce
