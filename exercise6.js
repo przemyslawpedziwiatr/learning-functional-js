4ae6db8b1eddf6621769ab0e59162a7ed5222ec6
pp <przemyslaw.pedziwiatr@gmail.com>
2019-02-02 10:22:11 +0100

    Best looking solution

--- exercise6.js
+++ exercise6.js
@@ -1,10 +1,6 @@
 function countWords(inputWords) {
     const reducer = (accumulator, curr) => {
-        if(accumulator[curr] !== undefined) {
-            accumulator[curr] += 1;
-        } else {
-            accumulator[curr] = 1;
-        }
+        accumulator[curr] = ++accumulator[curr] || 1;
         return accumulator;
     };
     return inputWords.reduce(reducer, {});
