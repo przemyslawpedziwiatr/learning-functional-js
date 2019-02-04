326050d018feeb9ec2e96b8a9add10e532f3a7a7
Przemyslaw Pedziwiatr <przemyslaw.pedziwiatr@gmail.com>
2019-02-04 14:25:41 +0100

    Exercise 8 duckCount done!

--- exercise8-duckcount.js
+++ exercise8-duckcount.js
@@ -1,8 +1,7 @@
 function duckCount() {
-    arguments.length = 0;
-    if(arguments[0] && Object.prototype
-
-    return 10;
+    return Array.prototype.filter.call(arguments, 
+        (arg) => Object.prototype.hasOwnProperty.call(arg, 'quack')
+    ).length;
 }
 
 module.exports = duckCount
